ec2fa4724fa174e87d00619fc86641fbdab767c4
Sebastian Kulinski <sebastian.elf.kulinski@gmail.com>
2021-09-14 08:35:30 +0200

    Add one more basic test case, refactor code for the regular input to use table tests approach

--- FizzBuzz/FizzBuzz_test.go
+++ FizzBuzz/FizzBuzz_test.go
@@ -5,19 +5,22 @@ import (
 	"testing"
 )
 
-func TestConvertNormalNumbers(t *testing.T) {
-	t.Run("should return 1 when 1 provided", func(t *testing.T) {
-		got := FizzBuzz.Convert(1)
-		expect := "1"
-		if got != expect {
-			t.Errorf("Got: %q want: %q", got, expect)
-		}
-	})
-	t.Run("should return 2 when 2 provided", func(t *testing.T) {
-		got := FizzBuzz.Convert(2)
-		expect := "2"
-		if got != expect {
-			t.Errorf("Got: %q want: %q", got, expect)
-		}
-	})
+func TestConvertNormalNumbersShould(t *testing.T) {
+	var testsValues = []struct {
+		name   string
+		input  int
+		expect string
+	}{
+		{"return 1 when 1 provided", 1, "1"},
+		{"return 2 when 2 provided", 2, "2"},
+		{"return 4 when 4 provided", 4, "4"},
+	}
+	for _, testValue := range testsValues {
+		t.Run(testValue.name, func(t *testing.T) {
+			got := FizzBuzz.Convert(testValue.input)
+			if got != testValue.expect {
+				t.Errorf("Got: %q want: %q", got, testValue.expect)
+			}
+		})
+	}
 }
