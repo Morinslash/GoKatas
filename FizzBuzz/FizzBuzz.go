e19a0fa1c8e572b971b7ae21e2089980d7dcd49b
Sebastian Kulinski <sebastian.elf.kulinski@gmail.com>
2021-09-14 08:42:03 +0200

    Add test cases for number divisible by 3 and 5, implement naive approach

--- FizzBuzz/FizzBuzz.go
+++ FizzBuzz/FizzBuzz.go
@@ -3,5 +3,11 @@ package FizzBuzz
 import "strconv"
 
 func Convert(number int) string {
+	if number%3 == 0 {
+		return "Fizz"
+	}
+	if number%5 == 0 {
+		return "Buzz"
+	}
 	return strconv.Itoa(number)
 }
--- FizzBuzz/FizzBuzz_test.go
+++ FizzBuzz/FizzBuzz_test.go
@@ -7,19 +7,59 @@ import (
 
 func TestConvertNormalNumbersShould(t *testing.T) {
 	var testsValues = []struct {
-		name   string
-		input  int
-		expect string
+		testName string
+		input    int
+		expected string
 	}{
 		{"return 1 when 1 provided", 1, "1"},
 		{"return 2 when 2 provided", 2, "2"},
 		{"return 4 when 4 provided", 4, "4"},
 	}
 	for _, testValue := range testsValues {
-		t.Run(testValue.name, func(t *testing.T) {
+		t.Run(testValue.testName, func(t *testing.T) {
 			got := FizzBuzz.Convert(testValue.input)
-			if got != testValue.expect {
-				t.Errorf("Got: %q want: %q", got, testValue.expect)
+			if got != testValue.expected {
+				t.Errorf("Got: %q want: %q", got, testValue.expected)
+			}
+		})
+	}
+}
+
+func TestConvertNumberDivisibleBy3Should(t *testing.T) {
+	var testsValues = []struct {
+		testName string
+		input    int
+		expected string
+	}{
+		{"return Fizz when 3 provided", 3, "Fizz"},
+		{"return Fizz when 6 provided", 6, "Fizz"},
+		{"return Fizz when 9 provided", 9, "Fizz"},
+	}
+	for _, testValue := range testsValues {
+		t.Run(testValue.testName, func(t *testing.T) {
+			got := FizzBuzz.Convert(testValue.input)
+			if got != testValue.expected {
+				t.Errorf("Got: %q want: %q", got, testValue.expected)
+			}
+		})
+	}
+}
+
+func TestConvertNumberDivisibleBy5Should(t *testing.T) {
+	var testsValues = []struct {
+		testName string
+		input    int
+		expected string
+	}{
+		{"return Buzz when 5 provided", 5, "Buzz"},
+		{"return Buzz when 10 provided", 10, "Buzz"},
+		{"return Buzz when 20 provided", 20, "Buzz"},
+	}
+	for _, testValue := range testsValues {
+		t.Run(testValue.testName, func(t *testing.T) {
+			got := FizzBuzz.Convert(testValue.input)
+			if got != testValue.expected {
+				t.Errorf("Got: %q want: %q", got, testValue.expected)
 			}
 		})
 	}
