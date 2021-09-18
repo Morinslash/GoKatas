a7f3b25e2acf1e3c38f8132dc71e81c5c86cf19f
Sebastian Kulinski <sebastian.elf.kulinski@gmail.com>
2021-09-18 12:23:08 +0200

    Add test cases for year divisible by 100 but not by 400, provide implementation

--- leapyear/leapyear.go
+++ leapyear/leapyear.go
@@ -1,8 +1,14 @@
 package leapyear
 
 func IsLeap(year int) bool {
+
+	if year%100 == 0 && year%400 != 0 {
+		return false
+	}
+
 	if year%4 == 0 {
 		return true
 	}
+
 	return false
 }
--- leapyear/leapyear_test.go
+++ leapyear/leapyear_test.go
@@ -18,9 +18,7 @@ func TestYearIsNotLeapIfNotDivisibleBy4(t *testing.T) {
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			got := leapyear.IsLeap(test.year)
-			if got != test.expected {
-				t.Errorf("Got: %v \n Expected: %v", got, test.expected)
-			}
+			assertGotEqualsCaseExpected(t, got, test)
 		})
 	}
 }
@@ -34,9 +32,7 @@ func TestYearIsLeapIfDivisibleBy4(t *testing.T) {
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			got := leapyear.IsLeap(test.year)
-			if got != test.expected {
-				t.Errorf("Got: %v \n Expected: %v", got, test.expected)
-			}
+			assertGotEqualsCaseExpected(t, got, test)
 		})
 	}
 }
@@ -48,9 +44,26 @@ func TestYearIsLeapIfDivisibleBy400(t *testing.T) {
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			got := leapyear.IsLeap(test.year)
-			if got != test.expected {
-				t.Errorf("Got: %v \n Expected: %v", got, test.expected)
-			}
+			assertGotEqualsCaseExpected(t, got, test)
 		})
+	}
+}
+
+func TestYearIsNotLeapIfDivisibleBy100ButNotBy400(t *testing.T) {
+	var testCases = []testCase{
+		{name: "return false if year 1800", year: 1800, expected: false},
+		{name: "return false if year 1900", year: 1900, expected: false},
+	}
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			got := leapyear.IsLeap(test.year)
+			assertGotEqualsCaseExpected(t, got, test)
+		})
+	}
+}
+
+func assertGotEqualsCaseExpected(t *testing.T, got bool, test testCase) {
+	if got != test.expected {
+		t.Errorf("\n Got: %v \n Expected: %v", got, test.expected)
 	}
 }
